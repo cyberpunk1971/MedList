10ba665af64d154bf9ab9a531d676541c1597e6b
cyberpunk1971 <adt71@cox.net>
2020-08-27 06:50:09 -0700

    Update dashboard.js
    Am able to ping API, now need to construct query, properly, to return data for manipulation in the DOM.

--- client/public/dashboard.js
+++ client/public/dashboard.js
@@ -2,21 +2,24 @@ const searchForm = document.getElementById('search-form');
 const API_KEY = 'kLvHt2puR01mwrVfJM2yyP2m7CkQ1KvuIAAERSrc';
 const SEARCH_URL = 'https://api.fda.gov/drug/label.json';
 
+
+
 const getApiData = (input, callback) => {
    const query = {
        url: SEARCH_URL,
-       data: {
-           search: `openfda.brand_name:${input}`,
+       body: {
+           search: `?openfda.brand_name:${input}`,
            limit: 4
        },
    };
-   fetch(SEARCH_URL, query.data, searchMeds)
-   .fail(() => {
-       alert('item not found');
-   });
+   fetch(SEARCH_URL, query.body)
+   .then(response => response.json())
+   .then(body => console.log(body));
 }
 
-searchForm.addEventListener('submit', event => {
+searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    const input = event.currentTarget.querySelector('#med-search').value;
+    getApiData(input);
 
 })
