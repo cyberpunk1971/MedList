184f2649a8c4e14417dc4784e7748cfabd0a6e8e
cyberpunk1971 <adt71@cox.net>
2020-08-27 21:31:11 -0700

    APi call working

--- client/public/dashboard.js
+++ client/public/dashboard.js
@@ -1,18 +1,19 @@
 const searchForm = document.getElementById('search-form');
 const API_KEY = 'kLvHt2puR01mwrVfJM2yyP2m7CkQ1KvuIAAERSrc';
-const SEARCH_URL = 'https://api.fda.gov/drug/label.json';
 
 
 
 const getApiData = (input, callback) => {
-   const query = {
-       url: SEARCH_URL,
-       body: {
-           search: `?openfda.brand_name:${input}`,
-           limit: 4
-       },
-   };
-   fetch(SEARCH_URL, query.body)
+    const SEARCH_URL = `https://api.fda.gov/drug/label.json?search=brand_name=${input}`;
+
+//    const req = {
+//        url: SEARCH_URL,
+//        body: {
+//            search: `?openfda.brand_name:${input}`,
+//            limit: 4
+//        },
+//    };
+   fetch(SEARCH_URL)
    .then(response => response.json())
    .then(body => console.log(body));
 }
@@ -21,5 +22,6 @@ searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const input = event.currentTarget.querySelector('#med-search').value;
     getApiData(input);
-
+    console.log(input);
+    console.log(event.target);
 })
